eb5cc3b5d4352b3c426d0af77c4dccc33e218dbe
Nathan Blake <developer@blake-ink.com>
2021-04-04 13:38:17 -0400

    add files from old repo, add gitignore

---
+++ README_out.md
@@ -0,0 +1,31 @@
+# Project Title
+
+## Table of Contents
+[Description](#description)
+
+<span id="description">
+## Description
+This is a brief description of the project. It is only a few lines to talk about what it does.
+</span>
+
+## Installation Instructions
+
+
+## Usage Information
+
+
+## Contribution Guidelines
+If you wish to contribute to this project, email me at hello@n8blake.com or open an issue on the GitHub page.
+
+## Test Instructions
+
+
+## License
+
+MIT License
+		
+
+## Questions
+Email me at [hello@n8blake.com](mailto:hello@n8blake.com)
+
+[Github](https://github.com/n8blake)  
---
+++ utils/generateMarkdown.js
@@ -0,0 +1,61 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {
+	return `<img src=https://img.shields.io/static/v1?label=License&message=${license}&color=blue>`;
+}
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {
+	if(!license){
+		return `<a href="">${renderLicenseBadge(license)}</a>`;
+	}
+}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {
+	let licenseStr = "";
+	if(license != 'none'){
+		licenseStr = `## License
+
+${license}
+		`;
+	}
+	return licenseStr;
+}
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+  return `# ${data.title}
+
+## Table of Contents
+[Description](#description)
+
+<span id="description">
+## Description
+${data.description}
+</span>
+
+## Installation Instructions
+${data.installation}
+
+## Usage Information
+${data.usage}
+
+## Contribution Guidelines
+${data.contribution}
+
+## Test Instructions
+${data.tests}
+
+${renderLicenseSection(data.license)}
+
+## Questions
+Email me at [${data.email}](mailto:${data.email})
+
+[Github](https://github.com/${data.github})  
+`;
+}
+
+module.exports = generateMarkdown;
